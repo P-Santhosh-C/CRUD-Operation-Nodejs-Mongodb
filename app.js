48e0eaaa44c3d00a7a60046c0302a96745f05aa7
Santhosh <89576117+P-Santhosh-C@users.noreply.github.com>
2021-11-15 11:23:26 +0530

    Updated

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require("express"),
+  app = express(),
+  port = process.env.PORT || 3000,
+  mongoose = require("mongoose"),
+  book = require("./restapi/models/bookModel"),
+  bodyParser = require("body-parser");
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/library_ms", {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require("./restapi/routes/bookRoutes");
+routes(app);
+
+app.use((req, res) => {
+  res.status(404).send({ url: req.originalUrl + " not found" });
+});
+
+app.listen(port);
